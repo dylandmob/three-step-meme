3bc9b10a88283f5f7560f5309326bf5d9f185d3d
Dylan Craig <dylancraig@Dylans-MBP.fios-router.home>
2019-06-12 14:16:45 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import zupage from "zupage";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [post, setPost] = useState({ images: [] });
+  const [lines, setLines] = useState([]);
+
+  useEffect(() => {
+    async function getPost() {
+      const post = await zupage.getCurrentPost();
+      setLines(post.body.split("\n"));
+      setPost(post);
+    }
+    getPost();
+  }, []);
+
+  return post.images.map((image, index) => (
+    <div className="item row" key={image.id}>
+      <img alt="meme" src={image.url} />
+      <h3>{lines[index]}</h3>
     </div>
-  );
+  ));
 }
-
-export default App;
